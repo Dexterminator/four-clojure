4a627b89bcc4857f4f2836160adb7a078aad2007
Dexter Gramfors <dexter.gramfors@gmail.com>
2016-02-28 20:32:27 +0100

    39 solved.

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -1,11 +1,19 @@
 (ns four-clojure.core
   (:gen-class))
 
-(defn max-value
-  [& args]
-  (reduce (fn [maximum new] (if (> new maximum) new maximum)) args))
+(defn my-interleave
+  [lst1 lst2]
+  (loop [x (seq lst1)
+         y (seq lst2)
+         interleaved []]
+    (if (and x y)
+      (recur (seq (rest x)) (seq (rest y)) (conj interleaved (first x) (first y)))
+      interleaved)))
 
-(= (max-value 1 8 3 4) 8)
-(= (max-value 1 8 3 4) 8)
-(= (max-value 30 20) 30)
-(max-value 1 2)
+
+(= (my-interleave [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))
+(= (my-interleave [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))
+(= (my-interleave [1 2] [3 4 5 6]) '(1 3 2 4))
+(= (my-interleave [1 2 3 4] [5]) [1 5])
+
+(rest [1 2 3])
--- src/four_clojure/solutions.clj
+++ src/four_clojure/solutions.clj
@@ -5,3 +5,11 @@
   (reduce (fn [maximum new] (if (> new maximum) new maximum)) args))
 
 
+;39
+(fn [lst1 lst2]
+  (loop [x (seq lst1)
+         y (seq lst2)
+         interleaved []]
+    (if (and x y)
+      (recur (seq (rest x)) (seq (rest y)) (conj interleaved (first x) (first y)))
+      interleaved)))
